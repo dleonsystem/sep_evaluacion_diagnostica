4679ac554a622e75dbb02d5c30ecbae11ebd78c8
pepenautamx <51543641+pepenautamx@users.noreply.github.com>
2025-11-25 14:39:02 -0600

    Add fallback home routes

--- web/frontend/src/app/app.routes.ts
+++ web/frontend/src/app/app.routes.ts
@@ -5,5 +5,14 @@ export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
   }
 ];
