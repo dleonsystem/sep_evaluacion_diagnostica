13e5fe9dbd19512670323cd0a37b53d6556ebdce
pepenautamx <51543641+pepenautamx@users.noreply.github.com>
2025-11-25 17:31:07 -0600

    Import CommonModule in inicio component

--- web/frontend/src/app/components/inicio/inicio.component.ts
+++ web/frontend/src/app/components/inicio/inicio.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
