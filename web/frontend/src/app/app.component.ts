1517bf2a745f9c99890d5f2b4b1345d72eac6dd9
pepenautamx <51543641+pepenautamx@users.noreply.github.com>
2025-11-26 14:47:15 -0600

    Mark AppComponent as standalone

--- web/frontend/src/app/app.component.ts
+++ web/frontend/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { NavComponent } from './shared/nav/nav.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, FormsModule, CommonModule, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
