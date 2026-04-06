51792e334a3eebcf3deea8a023e5ae397fb8836f
pepenautamx <51543641+pepenautamx@users.noreply.github.com>
2026-04-06 13:23:21 -0600

    Updating coverage thresholds to 10% in jest.config.cjs

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  // other configurations...
+  coverageThreshold: {
+    global: {
+      branches: 10,
+      functions: 10,
+      lines: 10,
+      statements: 10,
+    },
+  },
+};
