19bf462fba015f5a3d56e0204fd619c590da97bf
MARONATTI06 <77314898+MARONATTI06@users.noreply.github.com>
2026-05-05 06:08:32 -0600

    docker prod

--- web/frontend/src/environments/environment.prod.ts
+++ web/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://168.255.101.230:5009', // Se puede configurar dinamicamente via window.EIA_API_URL o se deja vacio para usar proxy
+  apiUrl: 'http://172.31.70.56:5009', // Se puede configurar dinamicamente via window.EIA_API_URL o se deja vacio para usar proxy
   graphqlPath: '/graphql'
 };
