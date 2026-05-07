ea064b4d53e114fa2e44ede730c90227f4a29505
MARONATTI06 <77314898+MARONATTI06@users.noreply.github.com>
2026-05-06 18:09:45 -0600

    Update environment.prod.ts

--- web/frontend/src/environments/environment.prod.ts
+++ web/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://172.31.70.56:5009', // Se puede configurar dinamicamente via window.EIA_API_URL o se deja vacio para usar proxy
+  apiUrl: 'http://qa-api.sirver.sep.gob.mx', // Se puede configurar dinamicamente via window.EIA_API_URL o se deja vacio para usar proxy
   graphqlPath: '/graphql'
 };
